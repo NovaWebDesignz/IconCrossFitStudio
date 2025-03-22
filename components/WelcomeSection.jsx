cf07782a7091ba606dfa9f73705fe0753c62a61f
NovaWebDesignz <novawebdesignz.com>
2025-03-22 18:19:26 +0530

    welcome section in xs mobile screen size responsivenesss update

--- components/WelcomeSection.jsx
+++ components/WelcomeSection.jsx
@@ -3,7 +3,7 @@ import { Cover } from "@/components/ui/cover";
 
 export default function WelcomeSection() {
   return (
-    <div className="relative w-full bg-sky-400 py-2 -mt-32 mb-20 flex justify-center items-center">
+    <div className="relative w-full bg-sky-400 py-2 -mt-40 sm:-mt-32 mb-28 flex justify-center items-center">
       <h1
         className="text-3xl md:text-4xl lg:text-4xl font-semibold text-center max-w-7xl mx-auto relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white"
       >
