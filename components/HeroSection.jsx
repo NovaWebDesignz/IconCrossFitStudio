9d2e5e2138bfd295a1ce5381e7f6d902032607d7
NovaWebDesignz <novawebdesignz.com>
2025-03-22 17:34:00 +0530

    new animation called TextGenerated animation from aceternaity

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import TextGenerateEffect from "./ui/TextGenerateEffect";
 
 const images = [
   "/images/Homepage_image1.jpg",
@@ -15,15 +16,17 @@ const images = [
  
 const HeroSection = () => {
   return (
-    <section className="relative w-full max-w-[94%] sm:max-w-[95%] md:max-w-[98%] h-[80vh] sm:h-[91vh] md:h-[100vh] flex items-center justify-end bg-gray-900 mt-[50px] md:mt-[30px] lg:mt-[8px] rounded-3xl overflow-hidden mx-auto">
+    <section className="relative w-full max-w-[94%] sm:max-w-[95%] md:max-w-[98%] h-[80vh] sm:h-[91vh] md:h-[100vh] flex items-center justify-end bg-gray-900 mt-[95px] md:mt-[30px] lg:mt-[8px] rounded-3xl overflow-hidden mx-auto">
       {/* Left Side Text */}
       <div className="absolute left-6 md:left-8 lg:left-14 xl:left-24 top-[14%] sm:top-[13%] md:top-[20%] lg:top-[15%] text-white max-w-[70%] sm:max-w-[65%] md:max-w-[60%] lg:max-w-[60%] xl:max-w-[40%] z-10">
         <p className="text-xs md:text-sm lg:text-md px-3 py-1 bg-gray-800 text-white font-semibold uppercase rounded-lg inline-block tracking-wider">
           Welcome to Icon Gym
         </p>
-        <h1 className="text-[35px] sm:text-[55px] lg:text-[80px] md:text-6xl font-extrabold leading-tight mt-4">
-          Energize Your Life <br /> with Our <span className="text-sky-400">Fitness Center</span>
-        </h1>
+        {/* Animated Text Effect */}
+        <TextGenerateEffect
+          words="Energize Your Life with Our Fitness Center"
+          className="text-[35px] sm:text-[55px] lg:text-[80px] md:text-6xl font-extrabold leading-tight mt-4"
+        />
         <button className="mt-6 px-6 py-3 bg-sky-500 hover:bg-sky-600 text-white font-semibold rounded-lg text-lg shadow-md transition-all">
           Discover More
         </button>
---
+++ components/ui/TextGenerateEffect.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "motion/react";
+import { cn } from "@/lib/utils";
+
+const TextGenerateEffect = ({ words, className, filter = true, duration = 0.5 }) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+        filter: filter ? "blur(0px)" : "none",
+      },
+      {
+        duration: duration || 1,
+        delay: stagger(0.2),
+      }
+    );
+  }, [scope.current]);
+
+  return (
+    <div className={cn("font-bold", className)}>
+      <motion.div ref={scope} className="mt-4 text-[35px] sm:text-[55px] lg:text-[80px] md:text-6xl font-extrabold mt-4 leading-snug tracking-wide">
+        {wordsArray.map((word, idx) => (
+          <motion.span
+            key={word + idx}
+            className="dark:text-white text-black opacity-0"
+            style={{ filter: filter ? "blur(10px)" : "none" }}
+          >
+            {word}{" "}
+          </motion.span>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default TextGenerateEffect;
