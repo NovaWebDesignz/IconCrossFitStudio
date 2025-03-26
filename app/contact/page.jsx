d8008957621799a2c2ff308a88790d87603cd736
NovaWebDesignz <novawebdesignz@gmail.com>
2025-03-27 00:33:24 +0530

    removed unused : location.jsx, membershipplane, space, successstories.jx files from the componets section and also ensure that no unneed file exist

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,10 @@
 import ContactUs from "@/components/contact/ContactUs";
-import Location from "@/components/contact/Location";
 import FAQ from "@/components/contact/FAQ";
 
 const ContactPage = () => {
   return (
     <div>
       <ContactUs />
-      
       <FAQ />
     </div>
   );
--- components/membership/space.jsx
+++
@@ -1,8 +0,0 @@
-const Space = () => {
-  return (
-    <div className="my-32 bg-[#111826]">
-    </div>
-  );
-};
-
-export default Space;
